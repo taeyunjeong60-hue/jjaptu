7ff2a89fe4475d43dddad18c83507093b89e0b3d
ci667702-star <taeyunjeong60@gmail.com>
2026-07-25 17:50:46 +0900

    .
    docs. apikey 환경 함수

--- 기본 이론/game_demo.py
+++ 기본 이론/game_demo.py
@@ -4,16 +4,17 @@ import random
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
 korean_con=["ㄱ", "ㄴ", "ㄷ", "ㄹ", "ㅁ", "ㅂ", "ㅅ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"]
 korean_gat=["ㅏ", "ㅑ", "ㅓ", "ㅕ", "ㅗ", "ㅛ", "ㅜ", "ㅠ", "ㅡ", "ㅣ", "ㅐ", "ㅒ", "ㅔ", "ㅖ", "ㅘ", "ㅙ", "ㅚ", "ㅝ", "ㅞ", "ㅟ", "ㅢ"]
-
 
 Userinput=input("""
 자음:{}
                     
 단어 입력:
                 """)
-apikey="8392370E3F14C500277E590A1F542B46"
+apikey= os.getenv("korean_dic_api_key")
+print(apikey)
 url = "https://stdict.korean.go.kr/api/search.do"
 
 params = {
