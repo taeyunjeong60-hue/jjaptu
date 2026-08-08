714b7e012eb7d76177e0f3f995c6e5c7a5a1bc44
ci667702-star <taeyunjeong60@gmail.com>
2026-08-08 16:32:46 +0900

    +README
    +멀티 기능 개발 중

--- jjaptu.server.py
+++ jjaptu.server.py
@@ -1,5 +1,6 @@
 #서버
 import socket
+import select
 
 client_socket=()
 
@@ -10,8 +11,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST,POST))
     s.listen()
     print("서버 시작")
+    readsocks=[s]
     while True:
-        conn,addr=s.accept()    
+        read,write,error=select.select([s],[],[])
+        for sock in read:
+            conn,addr=s.accept()    
+        
     
 
     
